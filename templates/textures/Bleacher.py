54e7281ecccdc79f3320822763b8319887d4ef54
OVNSAME <162483465+OVNSAME@users.noreply.github.com>
2024-08-03 15:49:52 +0300

    Added new textures templates

---
+++ templates/textures/Bleacher.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import os
+
+def bleach(folder):
+  for filename in os.listdir(folder):
+    if filename.endswith(".png"):
+      filepath = os.path.join(folder, filename)
+      try:
+        image = Image.open(filepath).convert("L")
+        image.save(filepath)
+      except Exception as e:
+        print(f"Error on file {filename}: {e}")
+
+if __name__ == "__main__":
+  bleach("C:\\Users\\Desktop\\templates") 
