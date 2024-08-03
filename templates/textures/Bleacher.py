079e1635887e170b2fb7634243ed85dd32dd2eaf
Ovnsame <162483465+OVNSAME@users.noreply.github.com>
2024-08-03 17:05:55 +0300

    Update Bleacher.py

--- templates/textures/Bleacher.py
+++ templates/textures/Bleacher.py
@@ -12,4 +12,5 @@ def bleach(folder):
         print(f"Error on file {filename}: {e}")
 
 if __name__ == "__main__":
-  bleach("C:\\Users\\Desktop\\templates") 
+  bleach("C:\\Users\\Desktop\\templates")
+# Changes in the image may not be accurate and must be manually corrected!
