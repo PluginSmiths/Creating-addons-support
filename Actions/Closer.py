bd0ea31ad2fb944416a6240f8dc1411404368246
OVNSAME <162483465+OVNSAME@users.noreply.github.com>
2024-11-16 20:50:24 +0300

    Update Closer.py

--- Actions/Closer.py
+++ Actions/Closer.py
@@ -1,14 +1,21 @@
 import os
 import subprocess
 
-processes = subprocess.check_output(['tasklist'], shell=True, universal_newlines=True).splitlines()
-list = ["javaw.exe"]
-javaw_processes = [process for process in processes if list in process]
+process_names = ["javaw.exe"]
 
-for process in javaw_processes:
-    process_id = process.split()[0]
-    try:
-        os.system(f"taskkill /F /PID {process_id}")
-        print(f"Process {process_id} stopped.")
-    except Exception as e:
-        print(f"Error: {process_id}: {e}")
+try:
+    processes_output = subprocess.check_output(['tasklist'], shell=True, universal_newlines=True)
+    running_processes = processes_output.splitlines()
+except subprocess.CalledProcessError as e:
+    print(f"Error on getting processes list: {e}")
+
+for process_name in process_names:
+    matching_processes = [p for p in running_processes if process_name in p]
+
+    for process in matching_processes:
+        try:
+            process_id = process.split()[1]
+            os.system(f"taskkill /F /PID {process_id}")
+            print(f"Process '{process_name}' with PID {process_id} stopped.")
+        except Exception as e:
+            print(f"Error on '{process_name}': {e}")
