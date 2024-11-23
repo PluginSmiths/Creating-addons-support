3edcc5f8434b8dc0ee840200b04228d8a50c7c05
OVNSAME <162483465+OVNSAME@users.noreply.github.com>
2024-11-23 20:57:43 +0300

    Update Archiever.py

--- Actions/Archiever.py
+++ Actions/Archiever.py
@@ -1,29 +1,33 @@
 import zipfile
 import os
-import rarfile
 import Workspace
 
-def create_archive(source_folder, destination_folder, zip_filename):
+def create_archive(source_folder, destination_folder, archive_filename):
     try:
-        zip_path = os.path.join(destination_folder, zip_filename)
-        with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        archive_path = os.path.join(destination_folder, archive_filename + ".zip")
+        with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for root, _, files in os.walk(source_folder):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    zip.write(file_path, arcname=file_path[len(source_folder) + 1:])
-        print(f"Archive created: {zip_path}")
+                    relative_path = os.path.relpath(file_path, source_folder)
+                    zipf.write(file_path, arcname=relative_path) 
+        print(f"Архив успешно создан: {archive_path}")
     except FileNotFoundError:
-        print(f"Folder {source_folder} did not found.")
+        print(f"Папка {source_folder} не найдена.")
     except Exception as e:
-        print(f"Error on creating archive: {e}")
+        print(f"Ошибка при создании архива: {e}")
 
 s = input()
 
 if "ME" in s:
-    create_archive(Workspace.get_repository_path() + "CAS_ME//", Workspace.get_desktop_path(), "CAS Mod Elements")
+    source = str(Workspace.get_repository_path()) + "CAS_ME//"
+    create_archive(source, Workspace.get_desktop_path(), "CAS Mod Elements")
 elif "API" in s:
-    create_archive(Workspace.get_repository_path() + "CAS_APIS//", Workspace.get_desktop_path(), "CAS API's")
+    source = str(Workspace.get_repository_path()) + "CAS_APIS//"
+    create_archive(source, Workspace.get_desktop_path(), "CAS API's")
 elif "P" in s:
-    create_archive(Workspace.get_repository_path() + "CAS_P//", Workspace.get_desktop_path(), "CAS Procedures")
-
+    source = str(Workspace.get_repository_path()) + "CAS_P//"
+    create_archive(source, Workspace.get_desktop_path(), "CAS Procedures")
+else:
+    print("Неизвестный тип архива.")
 
